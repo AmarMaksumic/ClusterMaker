cde72b87ffbf6f3edce8c40040530bed0befac52
Hali Huang <haliwhang4444@gmail.com>
2021-04-13 20:36:47 -0400

    added openlayers map with clustered markers

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,17 @@
+import 'ol/ol.css';
+import {Map, View, Feature} from 'ol';
+import * as geom from 'ol/geom'
+import * as proj from 'ol/proj'
+import {Cluster, OSM, Vector as VectorSource} from 'ol/source';
+import {Tile as TileLayer, Vector as VectorLayer} from 'ol/layer';
+import {
+  Circle as CircleStyle,
+  Fill,
+  Stroke,
+  Style,
+  Text
+} from 'ol/style';
+
 const fileSelector = document.getElementById('csv-input');
 
 if (fileSelector){
@@ -9,50 +23,100 @@ if (fileSelector){
   });
 } 
 
+let map;
+let layer;
+var locations: Array<Feature> = [];
+const distance = <HTMLFormElement>document.getElementById('distance');
+
 
 
 function readCSV(file: File){
     var reader = new FileReader()
     reader.onload = (event) => {
       if(event.target){
-        var data = event.target.result
-        console.log(data)
+        var data = <string>event.target.result
+        if(data){
+          var lines = data.split("\n")
+          console.log(lines.length)
+          lines.shift()
+          locations = []
+          for(let line of lines){
+            var latlong = line.split(",")
+            var location: Feature = new Feature({
+              geometry: new geom.Point(proj.fromLonLat([parseFloat(latlong[1]), parseFloat(latlong[0])]))
+            })
+            locations.push(location)
+          }
+          console.log(locations.length)
+          labelMap(locations)
+        }
       }
     }
     reader.readAsText(file)  
 }
 
-let map: google.maps.Map;
-const center: google.maps.LatLngLiteral = {lat: 30, lng: -110};
 
-
-function labelMap(locations: Array<google.maps.LatLngLiteral>){
-    const map = new google.maps.Map(
-        document.getElementById("map") as HTMLElement,
-        {
-          zoom: 3,
-          center: center
-        }
-      );
-    
-      // Create an array of alphabetical characters used to label the markers.
-      const labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    
-      // Add some markers to the map.
-      // Note: The code uses the JavaScript Array.prototype.map() method to
-      // create an array of markers based on a given "locations" array.
-      // The map() method here has nothing to do with the Google Maps API.
-      const markers = locations.map((location, i) => {
-        return new google.maps.Marker({
-          position: location,
-          label: labels[i % labels.length],
-        });
-      });
-    
-      // Add a marker clusterer to manage the markers.
-      // @ts-ignore MarkerClusterer defined via script
-      new MarkerClusterer(map, markers, {
-        imagePath:
-          "https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m",
-      });
+function initMap(){
+  map = new Map({
+    target: 'map',
+    layers: [
+      new TileLayer({
+        source: new OSM()
+      })
+    ],
+    view: new View({
+      center: proj.fromLonLat([-73.1, 40.763]),
+      zoom: 12,
+      // minZoom: 9,
+    })
+  });
 }
+
+
+function labelMap(locations: Array<Feature>){
+  var vector_source = new VectorSource({
+    features: locations
+  })
+  var cluster_source = new Cluster({
+    source: vector_source,
+    distance: parseInt(distance.value, 10),
+  })
+  var styleCache = {};
+
+  layer = new VectorLayer({
+    source: cluster_source,
+    style: function (feature) {
+      var size = feature.get('features').length;
+      var style = styleCache[size];
+      if (!style) {
+        style = new Style({
+          image: new CircleStyle({
+            radius: 8,
+            stroke: new Stroke({
+              color: '#fff',
+            }),
+            fill: new Fill({
+              color: '#3399CC',
+            }),
+          }),
+          text: new Text({
+            text: size.toString(),
+            fill: new Fill({
+              color: '#00000',
+            }),
+          }),
+        });
+        styleCache[size] = style;
+      }
+      return style;
+    },
+  });
+  map.addLayer(layer);
+  distance.style.display = "block"
+  distance.addEventListener('input', (event) => {
+    cluster_source.setDistance(parseInt(distance.value, 10));
+  });
+  
+}
+
+initMap()
--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,15 @@ module.exports = {
   },
   module: {
     rules: [
-      { test: /\.(ts|tsx)$/, loader: 'awesome-typescript-loader' }
+      { test: /\.(ts|tsx)$/, loader: 'awesome-typescript-loader' },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader',
+        ],
+      }
     ]
-  }
+  },
+  devtool: "eval-cheap-source-map"
 };
